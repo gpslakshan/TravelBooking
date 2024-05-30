62c67ddee8dbc9a6da5d62a3414e8808348ab3e9
Sachin <sachinlakshan04@gmail.com>
2024-05-30 19:40:30 +0530

    Register API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -11,9 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express test endpoint" });
-});
+app.use("/api/users", userRoutes);
 
 app.listen(5001, () => {
   console.log("Server is running on http://localhost:5001");
