2a563694e5a42e056a6e01e1c984ce98487b5a0d
Sachin <sachinlakshan04@gmail.com>
2024-06-01 18:54:06 +0530

    Sign-in functionality

---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,80 @@
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import * as apiClinet from "../api-client";
+import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
+
+export type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>();
+
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
+
+  const mutation = useMutation({
+    mutationFn: apiClinet.signIn,
+    onSuccess: () => {
+      showToast({ message: "Sign in Successful!", type: "SUCCESS" });
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold">Sign In</h2>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        />
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters long",
+            },
+          })}
+        />
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl rounded-md"
+        >
+          Sign In
+        </button>
+      </span>
+    </form>
+  );
+};
+
+export default SignIn;
