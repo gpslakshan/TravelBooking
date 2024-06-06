ad761e909de35e78ab6bbd59e6d781986c7eddc2
Sachin <sachinlakshan04@gmail.com>
2024-06-06 17:32:12 +0530

    Search Result Card

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useSearchContext } from "../contexts/SearchContext";
 import * as apiClinet from "../api-client";
 import { useState } from "react";
+import SearchResultsCard from "../components/SearchResultsCard";
 
 const Search = () => {
   const search = useSearchContext();
@@ -21,7 +22,30 @@ const Search = () => {
     queryFn: () => apiClinet.searchHotels(searchParams),
   });
 
-  return <div>Search Page</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by
+          </h3>
+          {/* TODO FILTERS */}
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
+          {/* TODO sort options */}
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultsCard hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
