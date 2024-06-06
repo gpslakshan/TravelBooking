b5735fbdd5cd1ca1b4afdf541e187605f773863b
Sachin <sachinlakshan04@gmail.com>
2024-06-06 16:26:37 +0530

    Fetch hotels in the search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,8 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import { useSearchContext } from "../contexts/SearchContext";
+import * as apiClinet from "../api-client";
+import { useState } from "react";
 
 const Search = () => {
   const search = useSearchContext();
-  console.log(search);
+  const [page, setPage] = useState<number>(1);
+
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+  };
+
+  const { data: hotelData } = useQuery({
+    queryKey: ["searchHotels", searchParams],
+    queryFn: () => apiClinet.searchHotels(searchParams),
+  });
 
   return <div>Search Page</div>;
 };
